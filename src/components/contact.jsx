bc2bc556ad46db58cfa4855ef29fb58e972b1eb3
Chandana jada <chandanaj405@gmail.com>
2025-12-18 12:20:26 +0530

    Update LinkedIn link in contact component

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -8,7 +8,7 @@ function Contact() {
     <h2>Contact Me</h2>
     <p>You can reach me via email at <a href="mailto:chandanaj405@gmail.com">chandanaj405@gmail.com</a></p>
     <p>Or connect on:</p>
-    <a className="link" href="#">LinkedIn</a>
+    <a className="link" href="linkedin.com/in/chandana-jada-24a4aa33b">LinkedIn</a>
 <a className="link" href="https://github.com/jada-chandana">GitHub</a>
 
   </div>
